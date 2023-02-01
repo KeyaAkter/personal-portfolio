298b99428de75a66c6ada47e62959cf1a9d03a2f
KeyaAkter <sanhatanvin12@gmail.com>
2023-02-01 21:04:09 +0600

    navbar & hero section added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,13 @@
+import Hero from "../components/Hero";
+import Bio from "./Bio";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home" id="home">
+      <Hero />
+      <Bio />
+    </div>
+  );
 };
 
 export default Home;
