2a7f97c983c8ddbaf8c1fbb5790450b1fcd7f0d6
KeyaAkter <sanhatanvin12@gmail.com>
2023-02-07 11:40:29 +0600

    about added

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,20 @@
 import { useRef } from "react";
+import { useHoverEffect } from "../hooks/useHoverEffect";
+import SectionTitle from "./SectionTitle";
+
+const data = {
+  img1: "https://res.cloudinary.com/djnti9d7n/image/upload/v1675342993/personal-portfolio/about-1_dxvzdu.jpg",
+  img2: "https://res.cloudinary.com/djnti9d7n/image/upload/v1675343009/personal-portfolio/about-2_bwad1l.jpg",
+};
 
 const About = () => {
   const aboutLeftRef = useRef(null);
+
+  useHoverEffect(aboutLeftRef, data.img1, data.img2);
+
   return (
     <div className="about container mx-auto mt-40" id="about">
-      <div className=" overflow-hidden">
-        <h2 className="section-title">Projects</h2>
-      </div>
+      <SectionTitle title={"About"} />
 
       <div className="about-wrapper mt-40 grid grid-cols-2 gap-20 overflow-hidden">
         <div className="about-left" ref={aboutLeftRef}></div>
@@ -25,7 +33,8 @@ const About = () => {
             dignissimos. Veniam est officia tenetur fuga vel consequatur
             accusantium libero ipsa.
           </p>
-          <p>
+
+          <p className="mt-10">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
             explicabo voluptates. Modi voluptate officia deleniti odio dolores
             suscipit quod ab, culpa nesciunt qui. Repellat incidunt laboriosam
@@ -39,6 +48,15 @@ const About = () => {
             dignissimos. Veniam est officia tenetur fuga vel consequatur
             accusantium libero ipsa.
           </p>
+
+          <a
+            href="https:// www.google.com"
+            target="_blank"
+            rel="noreferrer"
+            className="inline-block mt-10 uppercase py-8 px-14 border border-white/20 rounded-full hover:bg-cyan-400/20 hover:border-cyan-400/20 duration-500"
+          >
+            Resume
+          </a>
         </div>
       </div>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,8 @@ import Hero from "../components/Hero";
 import Bio from "./Bio";
 import Projects from "./Projects";
 import Skills from "./Skills";
-// import About from "./About";
+import About from "./About";
+import Contact from "./Contact";
 
 const Home = () => {
   return (
@@ -11,7 +12,8 @@ const Home = () => {
       <Bio />
       <Projects />
       <Skills />
-      {/* <About /> */}
+      <About />
+      <Contact />
     </div>
   );
 };
